a18447daaa8210372f6d2ad64b47ebe2ec22e4de
Daniel Jorge de Souza <danjorge@gmail.com>
2015-09-28 15:26:12 -0300

    mais alterações nos scripts

--- scriptsAgendamentoUnifor/scriptsAgendamentoUnifor.sql
+++ scriptsAgendamentoUnifor/scriptsAgendamentoUnifor.sql
@@ -49,4 +49,4 @@ insert into tb_status values (1, 'SOLICITAÇÃO CADASTRADA'), (2, 'SOLICITAÇÃO
 insert into configuracoes values (1,5), (1,6), (1,7), (1,8), (1,9), (1,10), (1, 11), (1, 12), (1,13), (1,14), (2,1), (2,2), (2,3), (2,4), (2,5), (2,6), (2,7), (2,8), (2,9), (2,10), (2,11), (2,12), (2,13), (2,14),
 (3,5), (3,6), (3,7), (3,8), (3,9)
 
-insert into perfis values (1,3)
+insert into perfis values (1,3), (2,1)
